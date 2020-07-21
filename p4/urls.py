ea06034eeceba2f6a7b44991c1e78b2935947f91
manju <manjukokane8888@gmail.com>
2020-07-21 22:52:51 +0530

    templates added

--- p4/urls.py
+++ p4/urls.py
@@ -22,5 +22,8 @@ urlpatterns = [
     path('home',views.home,name='home'),
     path('admin/', admin.site.urls),
     path('',views.demo,name='demo'),
+    path('home1',views.home1,name="home1"),
+    path('home2',views.home2,name="home2"),
+    path('home3',views.home3,name="home3"),
     
 ]
--- p4/views.py
+++ p4/views.py
@@ -10,3 +10,13 @@ def index(request):
 
 def home(request):
     return render(request,"demo/home.html")
+
+def home1(request):
+    return render(request,'demo/home1.html',context={"data":"manju","name":"rohit"})     
+
+def home2(request):
+    fruits = ['banana','mango','orange','kiwi','apple']
+    return render(request,'demo/home2.html',{'fruits':fruits})  
+
+def home3(request):
+    return render(request,"demo/home3.html",{"a":15,"b":50})
